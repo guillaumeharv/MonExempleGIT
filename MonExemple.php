23606213ca0db4648ac54743cae3b0127c98a1f9
guillaumeharv <guillaumeharv@gmail.com>
2022-10-19 14:03:46 -0400

    J'ai ajouté des choses

--- MonExemple.php
+++ MonExemple.php
@@ -1,10 +1,6 @@
 <?php 
     echo "Yes!";
-<<<<<<< HEAD
     echo "test";
-=======
-    echo "Un autre conflit";
-    echo "allo?";
->>>>>>> 1a53283d63d294b76b780bac05ebd8fed2178dcb
+echo "J'ajoute des lignes yay";
     echo "Ok, j'ai une autre modif à faire.";
 ?>
