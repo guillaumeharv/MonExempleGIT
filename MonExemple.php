8009c85a3cbb59bdcb33eda16aa07e5e81008b0a
Guillaume Harvey <guillaumeharv@gmail.com>
2023-01-23 12:46:55 -0500

    Modif laptop

--- MonExemple.php
+++ MonExemple.php
@@ -1,9 +1,5 @@
 <?php 
     echo "Yes!";
     echo "Ceci est une autre ligne...";
-<<<<<<< HEAD
-    echo "Ceci est sur l'ordinateur.";
-=======
     echo "Ceci vient du laptop yes.";
->>>>>>> 34b8146b262bb47e73bedc19ae7bfabc2f6b74f1
 ?>
