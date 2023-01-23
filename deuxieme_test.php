af9ec403890f86a31c8b6f0b18e1d4a9b976bc66
Guillaume Harvey <guillaumeharv@gmail.com>
2023-01-23 13:20:13 -0500

    Changement for et if

--- deuxieme_test.php
+++ deuxieme_test.php
@@ -1,7 +1,7 @@
 <?php 
-    if($variable == 1)
+    if($variable == 8)
         echo "test";
 
-    for($i = 0; $i < 3; $i++)
+    for($i = 0; $i < 2; $i++)
         echo "nouvelle ligne sur l'ordinateur";
 ?>
